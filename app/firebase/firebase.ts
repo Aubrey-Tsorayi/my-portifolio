296d24e0417b9af2ff1fa9acc6f443e375c4e6c3
tutsirayit@gmail.com <tutsirayit.tt@gmail.com>
2025-01-15 09:36:15 +0200

    fixed build errorss

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, Analytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,10 +14,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-let analytics: any;
+let analytics: Analytics | null = null;
+
 // Initialize Analytics and get a reference to the service
 if (typeof window !== 'undefined') {
-	analytics = getAnalytics(app);
+  analytics = getAnalytics(app);
 }
 
-export {analytics}
+export { analytics };
